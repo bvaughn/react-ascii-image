805c4a899ba6b7a9c45bf3d77759ec33955d016d
Brian Vaughn <brian.david.vaughn@gmail.com>
2016-05-26 18:20:48 -0700

    Updated demo to use custom image URL

--- source/AsciiImage/AsciiImage.example.js
+++ source/AsciiImage/AsciiImage.example.js
@@ -3,18 +3,7 @@ import React, { Component } from 'react'
 import AsciiImage, { DEFAULT_CHARACATERS } from './AsciiImage'
 import styles from './AsciiImage.example.css'
 
-const data = [
-  {
-    className: styles.MatrixImage,
-    image: 'http://crossorigin.me/https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg',
-    title: 'JPG'
-  },
-  {
-    className: styles.GoogleImage,
-    image: 'http://crossorigin.me/http://www.gstatic.com/images/branding/googlelogo/2x/googlelogo_color_284x96dp.png',
-    title: 'PNG (with transparency)'
-  }
-]
+const CROSS_ORIGIN_BLOCKER = 'http://crossorigin.me/'
 
 export default class AsciiImageExample extends Component {
   constructor (props) {
@@ -25,7 +14,8 @@ export default class AsciiImageExample extends Component {
       blockSize: 4,
       characters: DEFAULT_CHARACATERS,
       fontSize: 8,
-      renderMode: 'inline-block'
+      renderMode: 'inline-block',
+      url: 'https://images.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
     }
 
     this._updateState = debounce(this._updateState.bind(this), 500)
@@ -35,10 +25,11 @@ export default class AsciiImageExample extends Component {
     this._onCharactersChange = this._onCharactersChange.bind(this)
     this._onFontSizeChange = this._onFontSizeChange.bind(this)
     this._onRenderModeChange = this._onRenderModeChange.bind(this)
+    this._onUrlChange = this._onUrlChange.bind(this)
   }
 
   render () {
-    const { animated, blockSize, characters, fontSize, renderMode } = this.state
+    const { animated, blockSize, characters, fontSize, renderMode, url } = this.state
 
     return (
       <div className={styles.AsciiImageExample}>
@@ -93,30 +84,31 @@ export default class AsciiImageExample extends Component {
           </label>
         </div>
 
-        {data.map((datum) => (
-          <div
-            className={styles.Datum}
-            key={datum.title}
-          >
-            <h3>{datum.title}</h3>
+        <div className={styles.Datum}>
+          <label>
+            URL: {' '}
+            <input
+              className={styles.UrlInput}
+              defaultValue={url}
+              onChange={this._onUrlChange}
+            />
+          </label>
+          {url && (
             <div className={styles.ImageRow}>
-              <img
-                className={datum.className}
-                src={datum.image}
-              />
+              <img src={url} />
               <AsciiImage
                 animated={animated}
                 animationInterval={100}
                 blockSize={blockSize}
                 characters={characters}
-                className={datum.className}
+                className={styles.AsciiImage}
                 fontSize={fontSize}
                 renderMode={renderMode}
-                url={datum.image}
+                url={`${CROSS_ORIGIN_BLOCKER}${url}`}
               />
             </div>
-          </div>
-        ))}
+          )}
+        </div>
       </div>
     )
   }
@@ -159,6 +151,12 @@ export default class AsciiImageExample extends Component {
   _onRenderModeChange (event) {
     this._updateState({
       renderMode: event.target.value
+    })
+  }
+
+  _onUrlChange (event) {
+    this._updateState({
+      url: event.target.value
     })
   }
 }
